50a1132e67c34959b573efa69c9b093eb2929210
rwnwn <45011018+rwnwn@users.noreply.github.com>
2018-12-07 20:14:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 client.on('message', message => {
     let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('$bc')){
+    if (message.content.startsWith('$bc-bot')){
     if(!message.author.id === '') return;
     message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
     client.users.forEach(m =>{
@@ -9,4 +12,4 @@ client.on('message', message => {
     }
     });
     
-    client.login(process.env.BOT_TOKEN); 
+    client.login(process.env.BOT_TOKEN);
